e8346c99a9cdb6b9271ee04b5433e00103f8a843
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-12-12 20:07:11 +0500

    fix: code review fixes

--- src/components/TablesPager.jsx
+++ src/components/TablesPager.jsx
@@ -2,58 +2,78 @@ import React, { Component } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import PropTypes from 'prop-types';
 import CountriesTable from './CountriesTable';
 import { Global } from '../data/CountriesStub.json';
+import { pagerConstants } from '../helpers/helpers';
 
 class TablesPager extends Component {
   constructor(props) {
     super(props);
-    ({ tablesData: this.tableData, data: this.dataFields } = this.props);
     this.state = {
       page: 0,
       pageSize: 3,
       currentField: 'TotalConfirmed',
     };
-
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(e) {
+  handlePageChange = (e) => {
     const { id } = e.currentTarget;
-    const { page: value, pageSize } = this.state;
-    const index = (value + (id === 'back' ? -1 : 1) + pageSize) % pageSize;
+    const nextPage = this.updatePageIndex(id);
+    this.updateCurrentField(nextPage);
+  }
+
+  updatePageIndex = (buttonId) => {
+    const { page: currPage, pageSize } = this.state;
+    const incOrDec = buttonId === pagerConstants.arrowBackId ? -1 : 1;
+    const nextPage = (currPage + incOrDec + pageSize) % pageSize;
     this.setState(
       {
-        page: index,
-        currentField: this.dataFields[index],
+        page: nextPage,
       },
-      () => {
+    );
+    return nextPage;
+  }
+
+  updateCurrentField = (nextPage) => {
+    const { data: fieldsData } = this.props;
+    this.setState(
+      {
+        currentField: fieldsData[nextPage],
       },
     );
   }
 
   render() {
     const { currentField } = this.state;
+    const { tablesData } = this.props;
     return (
       <div>
         <div className="tables_pager">
           <div key={currentField}>
+            <h3>{currentField}</h3>
             <div className="global">
               <span>Global &nbsp;</span>
               <span>{Global[currentField]}</span>
             </div>
-            <CountriesTable data={this.tableData} field={currentField} />
+            <CountriesTable data={tablesData} field={currentField} />
           </div>
         </div>
         <nav className="tables_pagination">
           <ul className="tables_pagination-ul">
             <li>
-              <IconButton onClick={(e) => this.handleChange(e)} id="back">
+              <IconButton
+                onClick={(e) => this.handlePageChange(e)}
+                id={pagerConstants.arrowBackId}
+              >
                 <ArrowBackIcon />
               </IconButton>
             </li>
             <li>
-              <IconButton onClick={(e) => this.handleChange(e)} id="forward">
+              <IconButton
+                onClick={(e) => this.handlePageChange(e)}
+                id={pagerConstants.arrowForwardId}
+              >
                 <ArrowForwardIcon />
               </IconButton>
             </li>
@@ -64,5 +84,10 @@ class TablesPager extends Component {
     );
   }
 }
+
+TablesPager.propTypes = {
+  tablesData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  data: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default TablesPager;
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,5 +1,8 @@
-const sortArray = (rows, field, acs) => {
+export const sortArray = (rows, field, acs) => {
   rows.sort((r1, r2) => (r1[field] - r2[field]) * (acs ? 1 : -1));
 };
 
-export default sortArray;
+export const pagerConstants = {
+  arrowBackId: 'back',
+  arrowForwardId: 'forward',
+};
