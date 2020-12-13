4a5f49a8c2f487ef7010257742f004fef51c7a38
babkinevgeny <babkinevgeny@gmail.com>
2020-12-13 15:25:58 +0300

    fix: solve eslint error with sortArray

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,6 +1,6 @@
-export const sortArray = (rows, field, acs = false) => {
-  return [...rows].sort((r1, r2) => (r1[field] - r2[field]) * (acs ? 1 : -1));
-};
+export const sortArray = (rows, field, acs = false) => (
+  [...rows].sort((r1, r2) => (r1[field] - r2[field]) * (acs ? 1 : -1))
+);
 
 export const pagerConstants = {
   arrowBackId: 'back',
