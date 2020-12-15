af6ecf624a15e3a633fb92f27f282f0fe7864ebe
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-12-15 21:13:46 +0500

    feat: add full screen component wrapper

---
+++ src/components/FullPageComponentWrapper/index.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import FullscreenIcon from '@material-ui/icons/Fullscreen';
+import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+class FullPageComponentWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullScreen: false,
+    };
+  }
+
+  onExpandButtonClick = () => {
+    const { fullScreen } = this.state;
+    this.setState({
+      fullScreen: !fullScreen,
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { fullScreen } = this.state;
+    const icon = fullScreen ? <FullscreenExitIcon /> : <FullscreenIcon />;
+    return (
+      <div className={`component_wrapper${fullScreen ? ' wrapper_fullscreen' : ''}`}>
+        <IconButton
+          className="page_expand"
+          onClick={() => this.onExpandButtonClick()}
+          id="fullScreen"
+        >
+          {icon}
+        </IconButton>
+        {children}
+      </div>
+    );
+  }
+}
+
+FullPageComponentWrapper.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default FullPageComponentWrapper;
