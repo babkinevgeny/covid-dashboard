657f6aea7c1b87b106fc410ce4c46dadcc813368
babkinevgeny <babkinevgeny@gmail.com>
2020-12-23 21:58:25 +0300

    fix: review changes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ import schoolLogo from './rs_school.svg';
 const Footer = () => (
   <footer className="footer">
     <div className="footer__column footer__column--left">
-      <h6>Command:</h6>
+      <h6>Team:</h6>
       <ul>
         <li><a href="https://github.com/lbratkovskaya">lbratkovskaya</a></li>
         <li><a href="https://github.com/babkinevgeny">babkinevgeny</a></li>
