396ec4d75d8fb4ffdd23ab677f301a6c339c765c
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-12-23 13:26:27 +0500

    feat: tests

---
+++ src/helpers/helper.test.js
@@ -0,0 +1,29 @@
+import moment from 'moment';
+import { getStartOfYear, getMaxValue, getRandomIntInclusive } from './helpers';
+
+describe('Utilities methods testing', () => {
+
+  it('Expect getStartOfYear return value to be moment', () => {
+    expect(moment.isMoment(getStartOfYear(new Date()))).toBe(true);
+  });
+
+  it('Expect getStartOfYear to return the first year\'s date', () => {
+    const yearStart = new Date('2020-01-01');
+    yearStart.setUTCFullYear(2020, 0, 1);
+    const returned = getStartOfYear(new Date());
+
+    expect(returned.date()).toEqual(moment.utc(yearStart).date());
+  });
+
+  it('Expect getMaxValue to return maximum', () => {
+    expect(getMaxValue([1,3,5,2,4])).toEqual(5);
+  });
+
+  it('Expect getRandomIntInclusive to fit upper bound', () => {
+    expect(getRandomIntInclusive(5, 20)).toBeLessThanOrEqual(20);
+  });
+
+  it('Expect getRandomIntInclusive to fit lower bound', () => {
+    expect(getRandomIntInclusive(8, 13)).toBeGreaterThanOrEqual(8);
+  });
+});
